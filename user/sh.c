20d5377578307bef1cb3df00fca7b267f26f9792
肖灿 <21373254@buaa.edu.cn>
2023-05-25 00:16:28 +0800

    实现光标移动和删除功能

--- user/sh.c
+++ user/sh.c
@@ -33,6 +33,17 @@ int _gettoken(char *s, char **p1, char **p2) {
 		return 0;
 	}
 
+	if (*s == '"') {
+		s++;
+		*p1 = s;
+		while (*s && *s != '"') {
+			s++;
+		}
+		*s = 0;
+		*p2 = s;
+		return 'w';
+	}
+
 	if (strchr(SYMBOLS, *s)) {
 		int t = *s;
 		*p1 = s;
@@ -75,6 +86,7 @@ int parsecmd(char **argv, int *rightpipe) {
 		case 0:
 			return argc;
 		case 'w':
+			// printf("%s\n", t);
 			if (argc >= MAXARGS) {
 				debugf("too many arguments\n");
 				exit();
@@ -137,10 +149,40 @@ int parsecmd(char **argv, int *rightpipe) {
 			user_panic("| not implemented");
 
 			break;
+		case ';':;
+			int left = fork();
+			if (left > 0) {
+				wait(left);
+				return parsecmd(argv, rightpipe);
+			} else {
+				return argc;
+			}
+			break;
+		case '&':;
+			int back = fork();
+			if (back == 0) {
+				return argc;
+			} else {
+				return parsecmd(argv, rightpipe);
+			}
+			break;
 		}
 	}
 
 	return argc;
+}
+
+void add_b(char *dst, char *src) {
+	int i;
+	for (i = 0; i < MAXNAMELEN && src[i] != 0; i++) {
+		dst[i] = src[i];
+	}
+	if (i+2 >= MAXNAMELEN) {
+		debugf("the file %s.b name is too long\n", dst);
+	}
+	dst[i] = '.';
+	dst[i+1] = 'b';
+	dst[i+2] = 0;
 }
 
 void runcmd(char *s) {
@@ -153,13 +195,24 @@ void runcmd(char *s) {
 		return;
 	}
 	argv[argc] = 0;
-
+	// printf("%s\n", argv[0]);
 	int child = spawn(argv[0], argv);
+	// printf("%d\n", child);
 	close_all();
 	if (child >= 0) {
+		// printf("waiting\n");
 		wait(child);
 	} else {
-		debugf("spawn %s: %d\n", argv[0], child);
+		// char add_b_name[MAXNAMELEN];
+		// add_b(add_b_name, argv[0]);
+		// // printf("%s\n", add_b_name);
+		// int child = spawn(add_b_name, argv);
+		// close_all();
+		// if (child >= 0) {
+		// 	wait(child);
+		// } else {
+			debugf("spawn %s: %d\n", argv[0], child);
+		// }
 	}
 	if (rightpipe) {
 		wait(rightpipe);
@@ -167,28 +220,187 @@ void runcmd(char *s) {
 	exit();
 }
 
+/****************************************************
+ * readLine
+ * beforeCur from 0 to beforeLen-1
+ * afterCur from afterLen-1 to 0
+ * but cant intput 27 ESC
+ * 暂时不支持delete
+*****************************************************/
+#define UP 0x4b00
+#define DOWN 0x4d00
+#define RIGHT 0x5000
+#define LEFT 0x4800
+
+#define printUp printf("%c%c%c", 27, 91, 65);
+#define printDown printf("%c%c%c", 27, 91, 66);
+#define printRight printf("%c%c%c", 27, 91, 67);
+#define printLeft printf("%c%c%c", 27, 91, 68);
+
+int inputLen, beforeLen, afterLen;
+char beforeCur[1024];
+char afterCur[1024];
+char back[1024];
+char space[1024];
+
+void initBS() {
+	int i;
+	if (back[0] == '\b' || space[0] == ' ') {
+		return;
+	}
+	for (i = 0; i < 1024; i++) {
+		back[i] = '\b';
+	}
+	for (i = 0; i < 1024; i++) {
+		space[i] = ' ';
+	}
+}
+void printBack(int num) {
+	back[num] = 0;
+	printf("%s", back);
+	back[num] = '\b';
+}
+void printSpace(int num) {
+	space[num] = 0;
+	printf("%s", space);
+	space[num] = ' ';
+}
+
+void updateCons(int cursor) {
+	// printf("\nbefore %d | after %d | total %d\n", beforeLen, afterLen, inputLen);
+	int i;
+	printBack(cursor);
+	printSpace(inputLen+1);
+	printBack(inputLen+1);
+
+	for (i = 0; i < beforeLen; i++){
+		printf("%c", beforeCur[i]);
+	}
+	for (i = afterLen-1; i >= 0; i--) {
+		printf("%c", afterCur[i]);
+	}
+	for (i = 0; i < afterLen; i++){
+		printLeft;
+	}
+}
+
+void backOrDelete(char op) {
+	if (inputLen == 0) {
+		return;
+	}
+	// if (op == '\b') {
+		if (beforeLen == 0) {
+			return;
+		}
+		beforeLen--;
+	// } else if (op == 0x7f) {
+	// 	if (afterLen == 0) {
+	// 		return;
+	// 	}
+	// 	afterLen--;
+	// }
+	inputLen--;
+	if (afterLen == 0) {
+		printf("\b \b");
+	} else {
+		updateCons(beforeLen+1);
+	}
+}
+
+int readDir() {
+	char inc = 0;
+	int r = 0;
+	r = read(0, &inc, 1);
+	if (inc == 91) {
+		r = read(0, &inc, 1);
+		switch (inc) {
+			case 'A':
+				return UP;
+			case 'B':
+				return DOWN;
+			case 'C':
+				return RIGHT;
+			case 'D':
+				return LEFT;
+			default:
+				debugf("read error: %d\n", r);
+				exit();
+		}
+	} else {
+		debugf("read error: %d\n", r);
+		exit();
+	}
+}
+
+void moveCursor(int direction) {
+	if (direction == UP) {
+		printDown;
+		return;
+	} else if (direction == DOWN) {
+		return;
+	} else if (beforeLen == inputLen 
+				&& direction == RIGHT) {
+		printLeft;
+		return;
+	} else if (beforeLen == 0 
+				&& direction == LEFT) {
+		printRight;
+		return;
+	}
+	
+	if (direction == RIGHT) {
+		beforeCur[beforeLen++] = afterCur[--afterLen];
+	} else if (direction == LEFT) {
+		afterCur[afterLen++] = beforeCur[--beforeLen];
+	}
+}
+
+void loadBuf(char *buf) {
+	int i;
+	int bufp = 0;
+	for (i = 0; i < beforeLen; i++){
+		buf[bufp++] = beforeCur[i];
+	}
+	for (i = afterLen-1; i >= 0; i--) {
+		buf[bufp++] = afterCur[i];
+	}
+	buf[bufp] = 0;
+}
+
 void readline(char *buf, u_int n) {
 	int r;
-	for (int i = 0; i < n; i++) {
-		if ((r = read(0, buf + i, 1)) != 1) {
+	beforeLen = 0;
+	afterLen = 0;
+	for (inputLen = 0; inputLen < n;) {
+		char inc = 0;
+		int dir = 0;
+		if ((r = read(0, &inc, 1)) != 1) {
 			if (r < 0) {
 				debugf("read error: %d\n", r);
 			}
 			exit();
 		}
-		if (buf[i] == '\b' || buf[i] == 0x7f) {
-			if (i > 0) {
-				i -= 2;
-			} else {
-				i = -1;
-			}
-			if (buf[i] != '\b') {
-				printf("\b");
-			}
+		//printf("\b \b");
+		//printf(" %d == 7:  %d\n", (char)inc, (char)inc == (char)7);
+		if (inc == 27) {
+			dir = readDir();
+			//printf("%x\n", inc);
 		}
-		if (buf[i] == '\r' || buf[i] == '\n') {
-			buf[i] = 0;
+		
+		// printf("\n%d\n", inc);
+		if (inc == '\r' || inc == '\n') {
+			loadBuf(buf);
 			return;
+		} else if (inc == '\b' || inc == 0x7f) {
+			backOrDelete(inc);
+		} else if (inc == 27) {
+			moveCursor(dir);
+		} else {
+			beforeCur[beforeLen++] = inc;
+			inputLen++;
+			if (afterLen != 0) {
+				updateCons(beforeLen);
+			}
 		}
 	}
 	debugf("line too long\n");
@@ -197,6 +409,11 @@ void readline(char *buf, u_int n) {
 	}
 	buf[0] = 0;
 }
+
+/****************************************************
+ * readLine
+ * End
+*****************************************************/
 
 char buf[1024];
 
@@ -209,6 +426,7 @@ int main(int argc, char **argv) {
 	int r;
 	int interactive = iscons(0);
 	int echocmds = 0;
+	initBS();
 	debugf("\n:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n");
 	debugf("::                                                         ::\n");
 	debugf("::                     MOS Shell 2023                      ::\n");
